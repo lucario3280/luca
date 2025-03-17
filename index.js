dd632f9e43c2986a29923bbfbf707ae82c9c804f
lucario3280 <luquiscigliano@gmail.com>
2025-03-17 17:33:03 -0300

    mi primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function eje1() {
+    let meses=["Enero", "Febrero", "Marzo", "Abril", "Junio", "Julio", "Agosto", "Septienbre", "Octubre", "Novienbre", "Dicienbre"]
+    for (let i = 0; i < meses.length; i++) {
+        console.log(meses[i])
+        
+    }
+    
+    
+}
+function eje1() {
+    var valores=[true, 5, false, "hola", "adios", 2];
+    
+}
